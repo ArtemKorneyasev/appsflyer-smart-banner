97ea9e446c5d5fd70a50d03488e709840116e7d3
Artem Korneyasev <artem.korneyasev@onetwotrip.com>
2025-03-14 12:23:04 +0300

    замена скрипта для плашки appsflyer

--- src/components/AppsFlyerSmartBanner/AppsFlyerSmartBanner.tsx
+++ src/components/AppsFlyerSmartBanner/AppsFlyerSmartBanner.tsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
 
-const getScript = ({ scriptId, webKey }: { scriptId: string; webKey: string }) => `
+// const getScript = ({ scriptId, webKey }: { scriptId: string; webKey: string; }) => `
+//   !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){
+//   (t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},
+//   t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.id="${scriptId}",
+//   o.src="https://websdk.appsflyer.com?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),
+//   p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "${webKey}"}});
+// `;
+
+const getScript = ({ scriptId, webKey }: { scriptId: string; webKey: string; }) => `
   !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){
-  (t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},
-  t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.id="${scriptId}",
-  o.src="https://websdk.appsflyer.com?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),
-  p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "${webKey}"}});
+  (t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))}
+  ,t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement,p=e.getElementsByTagName[0],o.async=1,o.id="${scriptId}",
+  o.src="https://websdk.appsflyer.com?"(c.length>0?"st="+c.split(",").sort().join(",")"&":"")+(i.length>0?"af_id="+i:""),
+  p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "${webKey}"}}); AF('banners', 'showBanner')
 `;
 
 export type AppsFlyerSmartBannerProps = Readonly<{
